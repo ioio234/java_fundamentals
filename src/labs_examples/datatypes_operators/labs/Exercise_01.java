7ab3bfb2a5419508b0385a6470be79732558f22a
Pong <zolaa007@gmail.com>
2024-10-03 12:43:53 +0100

    New Exercise_01

--- src/labs_examples/datatypes_operators/labs/Exercise_01.java
+++ src/labs_examples/datatypes_operators/labs/Exercise_01.java
@@ -8,16 +8,18 @@ package labs_examples.datatypes_operators.labs;
  *      1. Declare an int, a double, a float and a char
  *      2. Assign appropriate values to each
  *      3. Print out each variable to the console
- *
- *
  */
-
 public class Exercise_01 {
 
     public static void main(String[] args) {
 
         // write code here
-
+        int a = 45;
+        double b = 40;
+        char c = 'a';
+        System.out.println("The value if a is: " + a);
+        System.out.println("The value of b is: " + b);
+        System.out.println("The value of c is: " + c);
     }
 }
 
