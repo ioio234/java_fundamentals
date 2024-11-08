5157724ba2aa92b1ae9674d9963b76a58461c9ef
Pong <zolaa007@gmail.com>
2024-11-08 20:47:43 +0100

    New Exercise_02

--- src/labs_examples/conditions_loops/labs/Exercise_02.java
+++ src/labs_examples/conditions_loops/labs/Exercise_02.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 2: Days of the week
  *
@@ -16,6 +18,22 @@ public class Exercise_02 {
     public static void main(String[] args) {
     
         // write completed here
+        Scanner scanner = new Scanner(System.in);
 
+        System.out.println("Enter a number from 1 to 7: ");
+
+        int number = scanner.nextInt();
+
+        if ((number < 1) || (number > 7)){
+            System.out.println("This number is beyond what was asked");
+        }else if (number == 1){
+            System.out.println("Monday");
+        }else if (number == 2){
+            System.out.println("Tuesday");
+        }else if (number == 3){
+            System.out.println("Wednesday");
+        }else {
+            System.out.println("Imagine");
+        }
     }
 }
