4de2855b3ebb79525579eed497474afa46bd5f19
Pong <zolaa007@gmail.com>
2024-11-08 21:11:14 +0100

    New Ex4

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -6,7 +6,15 @@ package labs_examples.conditions_loops.labs;
  *      Using a "for-loop", print out all even numbers from 1-100.
  *
  */
-
+import java.util.Scanner;
 public class Exercise_04 {
+    public static void main(String[] args) {
 
+        for (int count = 0; count <= 100; count++){
+            if (count % 2 == 0){
+                System.out.println(count);
+            }
+
+        }
+    }
 }
