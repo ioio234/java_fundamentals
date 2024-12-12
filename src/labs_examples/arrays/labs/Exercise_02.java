db8479c0e9abbdeb104b4f83e747d74a3b84aab4
Pong <zolaa007@gmail.com>
2024-12-12 15:57:42 +0100

    New Ex3

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -7,14 +7,24 @@ package labs_examples.arrays.labs;
  *      element.
  *
  */
-
+import java.util.Scanner;
 public class Exercise_02 {
 
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        sc.close();
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
         // write code here
+
+        for (int i = 0; i < array.length; i++){
+            if(array[i] == num){
+                System.out.println("number found and it is at index: " + i );
+                break;
+            }
+        }
 
 
     }
