e148a264fac4b9bcd55dbd0a848967b32f5d4380
Pong <zolaa007@gmail.com>
2024-12-12 15:43:15 +0100

    New Exercise_01

--- src/labs_examples/arrays/labs/Exercise_01.java
+++ src/labs_examples/arrays/labs/Exercise_01.java
@@ -11,7 +11,35 @@ import java.util.Arrays;
  *      Print the results to the console.
  *
  */
+import java.util.Scanner;
+
 
 public class Exercise_01 {
 
+    public static void main(String[] args){
+
+        System.out.print("Insert 10 numbers:");
+        int[] nums = new int[10];
+        Scanner sc = new Scanner(System.in);
+
+        for (int i = 0; i < nums.length; i++) {
+            int num = sc.nextInt();
+            nums[i] = num;
+        }
+        sc.close();
+        System.out.println("Your numbers are: ");
+
+        int sum = 0;
+
+        for (int i = 0; i < nums.length; i++){
+            sum = sum + nums[i];
+        }
+
+        float media = (float) sum / nums.length;
+
+        System.out.println(sum);
+        System.out.println(media);
+
+
+    }
 }
